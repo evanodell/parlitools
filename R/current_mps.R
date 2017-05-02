37e6297e9f648a8664166a2b5de036925aee3191
EvanOdell <evanodell91@gmail.com>
2017-05-02 15:30:35 +0100

    update docs

--- R/current_mps.R
+++ R/current_mps.R
@@ -14,9 +14,17 @@ current_mps <- function(tidy=TRUE){
   
   suppressMessages(constit <- hansard::constituencies(tidy=tidy))
   
-  if(packageVersion("mnis")>"0.2.5") {
+  if(packageVersion("mnis")>"0.2.3") {
   
   current <- mnis::mnis_eligible(house="commons", tidy=tidy)
+  
+  if(.Platform$OS.type=="windows"){
+    
+    current$member_from <- stringi::stri_trans_general(current$member_from, "latin-ascii")
+    
+    current$member_from <- gsub("Ynys MA\U00B4n", "Ynys M\U00F4n", current$member_from)
+    
+  }
   
   } else {
     
