3051793ecba38e9036da13648812bbf2327786cb
EvanOdell <evanodell91@gmail.com>
2017-05-07 09:42:34 +0100

    tweak to keep maps in place in vignette

--- testing.R
+++ testing.R
@@ -2,38 +2,82 @@ library(leaflet)
 library(sf)
 library(dplyr)
 library(hansard)
+library(mnis)
+library(parlitools)
 
-west_hex_map <- parlitools::west_hex_map #Base map
+west_hex_map <- parlitools::west_hex_map
 
-trump_no <- epetition(ID = 648278, by_constituency=TRUE) #Download anti-inviting Trump signatures
+party_colour <- parlitools::party_colour
 
-west_trump_no <- left_join(west_hex_map, trump_no, by = "gss_code") #Joining to base map
+mps <- mps_on_date("2017-04-19")
 
-pal = colorNumeric("Blues", trump_no$number_of_signatures)
+mps_colours <- left_join(mps, party_colour, by = "party_id") #Join to current MP data
 
-label_no <- paste0(
-  "<strong>", west_trump_no$constituency_name, "</strong>", "</br>",
-  "Signatures: ", west_trump_no$number_of_signatures
+west_hex_map <- left_join(west_hex_map, mps_colours, by = "gss_code") #Join colours to hexagon map
+
+# Creating map labels
+labels <- paste0(
+  "<strong>", west_hex_map$constituency_name, "</strong>", "</br>",
+  west_hex_map$party_name, "</br>",
+  west_hex_map$display_as, "</br>",
+  "Most Recent Result: ", west_hex_map$result_of_election, "</br>",
+  "Current Majority: ", west_hex_map$majority
 ) %>% lapply(htmltools::HTML)
 
-leaflet(options=leafletOptions(
-  dragging = FALSE, zoomControl = FALSE,
-  minZoom = 6, maxZoom = 6,
+# Creating the map itself
+map <- leaflet(options=leafletOptions(
+  dragging = FALSE, zoomControl = FALSE, tap = FALSE,
+  minZoom = 6, maxZoom = 6, maxBounds = list(list(2.5,-7.75),list(58.25,50.0)),
   attributionControl = FALSE),
-  west_trump_no) %>%
+  west_hex_map) %>%
   addPolygons(
     color = "grey",
     weight=0.75,
     opacity = 0.5,
     fillOpacity = 1,
-    fillColor = ~pal(number_of_signatures),
-    label = label_no) %>%
-  addLegend("topright", pal = pal, values = ~number_of_signatures,
-            title = "Number of Signatures",
-            opacity = 1) %>% 
+    fillColor = ~party_colour,
+    label=labels)  %>% 
   htmlwidgets::onRender(
     "function(x, y) {
         var myMap = this;
         myMap._container.style['background'] = '#fff';
-    }")%>% 
-  mapOptions(zoomToLimits = "first")
+    }")
+
+
+bounds <- input$mymap_bounds
+latRng <- range(bounds$north, bounds$south)
+lngRng <- range(bounds$east, bounds$west)
+
+library(tilegramsR)
+library(leaflet)
+devtools::install_github('bhaskarvk/leaflet.extras')
+library(leaflet.extras)
+
+getLeafletOptions <- function(minZoom, maxZoom, ...) {
+  leafletOptions(
+    crs = leafletCRS("L.CRS.Simple"),
+    minZoom = minZoom, maxZoom = maxZoom,
+    dragging = FALSE, zoomControl = FALSE,
+    tap = FALSE,
+    attributionControl = FALSE , ...)
+}
+
+getFactorPal <- function(f) {
+  colorFactor(colormap::colormap(
+    colormap = colormap::colormaps$hsv,
+    nshades = length(f)), f)
+}
+
+leaflet(
+  tilegramsR:: sf_NPR1to1,
+  options= getLeafletOptions(-1.5, -1.5)) %>%
+  addPolygons(
+    weight=2,color='#000000', group = 'states',
+    fillOpacity = 0.6, opacity = 1, fillColor= ~getFactorPal(state)(state),
+    highlightOptions = highlightOptions(weight = 4)) %>%
+  addLabelOnlyMarkers(
+    data=sf_NPR1to1.centers,
+    label = ~as.character(state),
+    labelOptions = labelOptions(
+      noHide = 'T', textOnly = T,
+      offset=c(-4,-10), textsize = '12px'))
