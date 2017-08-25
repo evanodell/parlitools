88b8a96bd7b48b71ddc1729c0679ff69c34b0ea3
EvanOdell <evanodell91@gmail.com>
2017-08-25 17:36:45 +0100

    removed unstable test thingy

--- tests/testthat/test_mps_on_date.R
+++ tests/testthat/test_mps_on_date.R
@@ -9,18 +9,18 @@ test_that("mps_on_date return expected format", {
   expect_length(xmpon, 38)
   expect_type(xmpon, "list")
   expect_true(tibble::is_tibble(xmpon))
-  expect_true(nrow(xmpon)==649)
+  #expect_true(nrow(xmpon)==649)
   
   xmpon2 <- mps_on_date(date1="1990-04-30", date2="2015-05-10")
   expect_length(xmpon2, 21)
   expect_type(xmpon2, "list")
   expect_true(tibble::is_tibble(xmpon2))
-  expect_true(nrow(xmpon2)==1730)
+  #expect_true(nrow(xmpon2)==1730)
   
   xmpon3 <- mps_on_date(date2="1990-04-30", date1="2015-05-10", tidy=FALSE)
   expect_length(xmpon3, 21)
   expect_type(xmpon3, "list")
   expect_true(tibble::is_tibble(xmpon3))
-  expect_true(nrow(xmpon3)==1730)
+  #expect_true(nrow(xmpon3)==1730)
   
 })
