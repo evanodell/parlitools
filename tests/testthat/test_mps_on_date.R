1d4bd6e5d0d3a50807614d94e872463341141294
Evan Odell <evanodell91@gmail.com>
2018-02-07 19:14:47 +0000

    test error fix

--- tests/testthat/test_mps_on_date.R
+++ tests/testthat/test_mps_on_date.R
@@ -5,12 +5,12 @@ test_that("mps_on_date return expected format", {
   
   skip_on_cran()
   
-  xmpon <- mps_on_date("2017-04-30")
-  expect_length(xmpon, 36)
+  xmpon <- mps_on_date("2018-01-30")
+  expect_length(xmpon, 38)
   expect_type(xmpon, "list")
   expect_true(tibble::is_tibble(xmpon))
   expect_true(is.na(xmpon$election_type[1])==FALSE)
-  #expect_true(nrow(xmpon)==649)
+  expect_true(nrow(xmpon)==649)
   
   xmpon2 <- mps_on_date(date1="1990-04-30", date2="2015-05-10")
   expect_length(xmpon2, 22)
