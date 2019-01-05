5dbd8527ffd72df02444f0e0bbcf1ff8a0374e69
Evan Odell <evanodell91@gmail.com>
2019-01-05 14:43:50 +0000

    add tibble to suggests

--- tests/testthat/test-council_seats.R
+++ tests/testthat/test-council_seats.R
@@ -2,7 +2,7 @@ context("test-council_seats")
 
 test_that("council_seats works", {
   summary <- council_seats(councillors = FALSE)
-  expect_true(tibble::is.tibble(summary))
+  expect_true(tibble::is_tibble(summary))
   expect_true(nrow(summary)==423)
   
   
